c51bbeca30f2431435dd874d7ac6f048fba3daa8
BandhaviC <bandhavic6@gmail.com>
2024-01-10 15:18:56 -0500

    updated config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'Todo-App-ReduxToolkit_latest',
   server: {
     proxy: {
       '/api': {
